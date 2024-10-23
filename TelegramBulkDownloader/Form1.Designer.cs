374fa07df90cb0df51d935e4c61885e551890a80
Федотов Михаил <qwoplakjos@gmail.com>
2024-10-23 17:31:25 +0300

    Добавьте файлы проекта.

---
+++ TelegramBulkDownloader/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TelegramBulkDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.apiIdTextBox = new System.Windows.Forms.TextBox();
+            this.apiHashTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.requestCodeButton = new System.Windows.Forms.Button();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.listBoxChannels = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // apiIdTextBox
+            // 
+            this.apiIdTextBox.Location = new System.Drawing.Point(93, 9);
+            this.apiIdTextBox.Name = "apiIdTextBox";
+            this.apiIdTextBox.Size = new System.Drawing.Size(163, 20);
+            this.apiIdTextBox.TabIndex = 0;
+            // 
+            // apiHashTextBox
+            // 
+            this.apiHashTextBox.Location = new System.Drawing.Point(93, 32);
+            this.apiHashTextBox.Name = "apiHashTextBox";
+            this.apiHashTextBox.Size = new System.Drawing.Size(163, 20);
+            this.apiHashTextBox.TabIndex = 1;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(93, 55);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(163, 20);
+            this.phoneNumberTextBox.TabIndex = 2;
+            // 
+            // requestCodeButton
+            // 
+            this.requestCodeButton.Location = new System.Drawing.Point(11, 82);
+            this.requestCodeButton.Name = "requestCodeButton";
+            this.requestCodeButton.Size = new System.Drawing.Size(245, 25);
+            this.requestCodeButton.TabIndex = 4;
+            this.requestCodeButton.Text = "Request Code";
+            this.requestCodeButton.Click += new System.EventHandler(this.requestCodeButton_Click);
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(11, 112);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(244, 25);
+            this.downloadButton.TabIndex = 5;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "API ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "API Hash:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Phone number:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(12, 151);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(244, 13);
+            this.labelStatus.TabIndex = 9;
+            this.labelStatus.Text = "Status:";
+            // 
+            // listBoxChannels
+            // 
+            this.listBoxChannels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxChannels.FormattingEnabled = true;
+            this.listBoxChannels.Location = new System.Drawing.Point(261, 9);
+            this.listBoxChannels.Name = "listBoxChannels";
+            this.listBoxChannels.Size = new System.Drawing.Size(288, 160);
+            this.listBoxChannels.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(555, 177);
+            this.Controls.Add(this.listBoxChannels);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.apiIdTextBox);
+            this.Controls.Add(this.apiHashTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.requestCodeButton);
+            this.Controls.Add(this.downloadButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telegram Media Downloader";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox apiIdTextBox;
+        private System.Windows.Forms.TextBox apiHashTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.Button requestCodeButton;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ListBox listBoxChannels;
+    }
+}
+
---
+++ TelegramBulkDownloader/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TL;
+using TL.Layer23;
+using WTelegram;
+
+
+namespace TelegramBulkDownloader
+{
+    public partial class Form1 : Form
+    {
+
+        string credsFile = "credentials.txt";
+        string downloadFolder = "downloads";
+        static Client client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(downloadFolder);
+            TryLoadCredentials();
+        }
+
+        string Config(string what)
+        {
+            switch (what)
+            {
+                case "api_id": return apiIdTextBox.Text; // Replace with your API ID
+                case "api_hash": return apiHashTextBox.Text; // Replace with your API Hash
+                case "phone_number": return phoneNumberTextBox.Text; // Replace with your phone number
+                case "verification_code": return Prompt.ShowDialog("Verification code:", "Code");
+
+                default: return null; // Default behavior for unhandled cases
+            }
+        }
+
+        private void SaveCredentials()
+        {
+            var id = Config("api_id");
+            var hash = Config("api_hash");
+            var number = Config("phone_number");
+
+            var resultText = id + "\n" + hash + "\n" + number;
+
+            File.WriteAllText(credsFile, resultText);
+        }
+
+        private void TryLoadCredentials()
+        {
+            if (File.Exists(credsFile))
+            {
+                try
+                {
+                    var creds = File.ReadAllText(credsFile).Split('\n');
+
+                    apiIdTextBox.Text = creds[0];
+                    apiHashTextBox.Text = creds[1];
+                    phoneNumberTextBox.Text = creds[2];
+                }
+                catch { }
+            }
+        }
+
+        private async void requestCodeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelStatus.Text = "Requesting auth code...";
+
+                client = new WTelegram.Client(Config);
+
+                var myself = await client.LoginUserIfNeeded();
+
+                labelStatus.Text = "Authenticated as: " + myself;
+
+                SaveCredentials();
+
+                Console.WriteLine($"We are logged in as {myself} (id {myself.id})");
+
+                var chats = await client.Messages_GetAllChats();
+                Console.WriteLine("This user has joined the following active chats:");
+
+                foreach (var chat in chats.chats)
+                {
+                    listBoxChannels.Items.Add(new ListItem { Text = chat.Value.Title + " -- " + chat.Value.ID, Value = chat.Value });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void downloadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (listBoxChannels.SelectedItem is ListItem selectedChat)
+                {
+                    InputPeer chat = null; // Initialize a variable to hold the InputPeer
+
+                    // Determine the type of selectedChat.Value and create the corresponding InputPeer
+                    switch (selectedChat.Value)
+                    {
+                        case Chat chatt:
+                            chat = selectedChat.Value as Chat;
+                            break;
+                        case Channel channel:
+                            chat = selectedChat.Value as Channel;
+                            break;
+                        default:
+                            MessageBox.Show("Selected item is not a valid chat or channel.");
+                            return; // Exit the method if the selected item is neither
+                    }
+
+
+
+                    for (int offset_id = 0; ;)
+                    {
+                        var messages = await client.Messages_GetHistory(chat, offset_id);
+                        if (messages.Messages.Length == 0) break;
+                        foreach (var msgBase in messages.Messages)
+                        {
+                            var from = messages.UserOrChat(msgBase.From ?? msgBase.Peer); // from can be User/Chat/Channel
+
+                            if (msgBase is TL.Message msg)
+                            {
+
+
+                                if (msg.media is MessageMediaDocument)
+                                {
+                                    var document = (msg.media as MessageMediaDocument).document as Document; // Cast to MessageMediaDocument
+                                    if (document != null)
+                                    {
+                                        var filename = document.Filename; // Use document's original filename, or build a name from document ID & MIME type:
+
+
+                                        if (string.IsNullOrEmpty(filename))
+                                        {
+                                            var mimeTypeParts = document.mime_type.Split('/');
+                                            filename = $"{document.id}.{mimeTypeParts[1]}"; // Build filename using ID and MIME type
+                                        }
+
+                                        labelStatus.Text = "Downloading " + filename;
+
+
+
+                                        while (File.Exists(downloadFolder + "/" + filename))
+                                        {
+                                            filename = "Copy_" + filename;
+                                        }
+
+
+                                        using (var fileStream = File.Create(downloadFolder + "/" + filename))
+                                        {
+                                            await client.DownloadFileAsync(document, fileStream);
+                                        }
+                                        Console.WriteLine("Download finished");
+                                    }
+                                }
+
+                                else
+                                if (msg.media is MessageMediaPhoto)
+                                {
+                                    var photo = ((MessageMediaPhoto)msg.media).photo as Photo;
+
+
+                                    var filename = $"{photo.id}TEMP.jpg";
+                                    labelStatus.Text = "Downloading " + filename;
+
+
+
+                                    var finalPath = downloadFolder + "/" + filename;
+
+
+
+                                    using (var fileStream = File.Create(finalPath))
+                                    {
+                                        var type = await client.DownloadFileAsync(photo, fileStream);
+
+                                        fileStream.Close(); // necessary for the renaming
+                                        Console.WriteLine("Download finished");
+                                        if (!(type is Storage_FileType.unknown) && !(type is Storage_FileType.partial))
+                                        {
+                                            var newFilename = $"{photo.id}.{type}";
+
+                                            while (File.Exists(downloadFolder + "/" + newFilename))
+                                            {
+                                                newFilename = "Copy_" + newFilename;
+                                            }
+
+                                            newFilename = downloadFolder + "/" + newFilename;
+
+
+                                            File.Move(finalPath, newFilename); // rename extension
+                                        }
+                                    }
+                                }
+                            }
+
+
+                        }
+                        offset_id = messages.Messages[messages.Messages.Count() - 1].ID;
+
+                    }
+
+
+
+                    MessageBox.Show("Download completed!");
+                }
+                else
+                {
+                    MessageBox.Show("Please select a chat to download media.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace + "\n---------\n" + ex.Message);
+            }
+        }
+
+        //private async Task DownloadMedia(FileLocation fileLocation)
+        //{
+        //    if (fileLocation == null) return;
+
+        //    // Get the file path and URL for download
+        //    var file = await client.DownloadFileAsync(fileLocation, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TelegramDownloads", ""));
+
+        //    // Ensure the directory exists
+        //    Directory.CreateDirectory(Path.GetDirectoryName(file));
+
+        //    using (var client = new WebClient())
+        //    {
+        //        client.DownloadFileCompleted += (s, e) =>
+        //        {
+        //            Console.WriteLine($"Downloaded: {fileLocation.FileName}");
+        //        };
+
+        //        // Start the download
+        //        await client.DownloadFileTaskAsync(new Uri(fileLocation.url), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TelegramDownloads", fileLocation.FileName));
+        //    }
+        //}
+    }
+    public class ListItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen,
+                FormBorderStyle = FormBorderStyle.FixedToolWindow
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox inputBox = new TextBox() { Left = 50, Top = 50, Width = 200 };
+            Button confirmation = new Button() { Text = "Ok", Left = 150, Width = 100, Top = 70 };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(inputBox);
+            prompt.Controls.Add(confirmation);
+            prompt.ShowDialog();
+            return inputBox.Text;
+        }
+    }
+}
